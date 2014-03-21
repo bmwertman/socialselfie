b82ef8c13ed71b923ab0fb45d2ca389cf4ecde7c
bmwertman <brad.wertman@leanmeanz.com>
2014-03-21 14:56:55 -0400

    cleanup

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,6 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                        # required
-    #:region                 => 'us-east-1',                  # optional, defaults to 'us-east-1'
-    # :host                   => 's3.example.com',             # optional, defaults to nil
-    # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory = ENV['AWS_BUCKET']
   config.fog_public     = true                                   # optional, defaults to true
